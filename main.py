337dfb56f8623a0dbe2160cd5273f58f3a94e61d
FunKeen <1987680880@qq.com>
2025-03-26 17:31:27 +0800

    changes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import torch
-import cv2
-
 from crnn.CRNNInferencer import CRNNInferencer
 from YOLOInferencer import YOLOInferencer
 
-yolo = YOLOInferencer('yolov5/runs/train/exp2/weights/best.pt', 'cpu')
-crnn = CRNNInferencer('crnn/crnn.pth', 'cpu')
-res = crnn(yolo(
-    'C:/Users/Keen/tempfile/CCPD2019/ccpd_base/01-90_88-196&604_384&663-386&662_213&658_204&598_377&602-0_0_20_4_31_29_32-112-27.jpg'))
-for i in res:
-    print(i)
+if __name__ == '__main__':
+    yolo = YOLOInferencer('yolov5/runs/train/exp2/weights/best.pt', 'cpu')
+    crnn = CRNNInferencer('crnn/crnn.pth', 'cpu')
+    res = crnn(yolo(
+        'C:/Users/Keen/tempfile/CCPD2019/ccpd_base/01-90_88-196&604_384&663-386&662_213&658_204&598_377&602-0_0_20_4_31_29_32-112-27.jpg'))
+    for i in res:
+        print(i)
